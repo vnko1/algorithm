e51fe9c8ad68ed708c354652cb64bd1c71e8846c
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2025-01-07 07:43:20 +0200

    added insert sort

--- sort.js
+++ sort.js
@@ -1,21 +1,42 @@
-// Bubble Sorting
+//* Bubble Sorting
 
-function bubbleSorting(arr) {
+// function bubbleSorting(arr) {
+//   const n = arr.length;
+
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//       if (arr[j] < arr[j + 1]) {
+//         // const temp = arr[j];
+//         // arr[j] = arr[j + 1];
+//         // arr[j + 1] = temp;
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// const numbers = [5, 3, 8, 4, 2];
+
+// console.log(bubbleSorting(numbers));
+
+//* Insertion sorting
+
+function insertSorting(arr) {
   const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let key = arr[i]; // 1=3
+    let j = i - 1; // 1=0
 
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] < arr[j + 1]) {
-        // const temp = arr[j];
-        // arr[j] = arr[j + 1];
-        // arr[j + 1] = temp;
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
+    while (j >= 0 && key < arr[j]) {
+      arr[j + 1] = arr[j];
+      j -= 1;
     }
+
+    arr[j + 1] = key;
   }
-  return arr;
 }
 
 const numbers = [5, 3, 8, 4, 2];
-
-console.log(bubbleSorting(numbers));
+insertSorting(numbers);
+console.log(numbers);
