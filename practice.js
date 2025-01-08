f1967cd91c07e28681f56742794fc8326e5194da
vnko1 <andreyvalenko@gmail.com>
2025-01-08 08:53:45 +0200

    added: practice

---
+++ practice.js
@@ -0,0 +1,16 @@
+function bubble(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+console.log(bubble([3, 1, 8, 5, 2]));
